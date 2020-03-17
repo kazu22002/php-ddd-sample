2f62f7ddafbf427d4f999db4b8ec5537d138c0c5
seo <kazu22002@gmail.com>
2020-03-17 21:37:02 +0900

    add: add repository prvider file.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        # 商用環境以外だった場合、SQLログを出力する
+        if (config('app.env') !== 'production') {
+//            \DB::listen(function ($query) {
+//                $sql = $query->sql;
+//                for ($i = 0; $i < count($query->bindings); $i++) {
+//                    $sql = preg_replace("/\?/", $query->bindings[$i], $sql, 1);
+//                }
+//                \Log::info("Query Time:{$query->time}s] $sql ");
+//            });
+        }
     }
 }
